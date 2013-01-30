5eb938ec0ee715779193b8491ac861d9d54cfd33
Ahmed Azaan <azaan@outlook.com>
2013-01-30 17:37:32 +0530

    Move cursor to end on focus

--- file_process.js
+++ file_process.js
@@ -1,23 +1,42 @@
 window.onload = function() {
-    
+
     var pad = document.getElementById('pad');
-    
+
+    //http://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/3866442#3866442
+    function setEndOfContenteditable(contentEditableElement)
+    {
+        var range,selection;
+        if(document.createRange)
+        {
+            range = document.createRange();
+            range.selectNodeContents(contentEditableElement);
+            range.collapse(false);
+            selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+
+        // else { IE < 8 }
+    }
+
     function handleWindowActive() {
         if( typeof localStorage.textpad_buffer != 'undefined') {
             pad.innerHTML = localStorage.textpad_buffer;
         } else {
             localStorage.textpad_buffer = pad.innerHTML;
         }
-    };
-    
+
+        setEndOfContenteditable(pad);
+    }
+
     function handleWindowBlur() {
         localStorage.textpad_buffer = pad.innerHTML;
-    };
-    
+    }
+
     function saveProgress() {
         localStorage.textpad_buffer = pad.innerHTML;
-    };
-    
+    }
+
     window.addEventListener("focus", handleWindowActive);
     window.addEventListener("blur", handleWindowBlur);
     window.onbeforeunload = function() {
