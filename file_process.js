5b1ed5b7fc1b2bca1fabf8437ff893289c46c004
Neetij Parekh <neetij@gmail.com>
2013-02-17 18:15:32 +0530

    Three text areas, save on keyup, privacy.
    Changed save method - previous method kept losing data when switching
    tabs.
    Split the content areas into three todo, in progress and done.
    For a nod to privacy, notes are visible only when tabbing into the
    viewport.

--- file_process.js
+++ file_process.js
@@ -1,46 +1,56 @@
 window.onload = function() {
 
-    var pad = document.getElementById('pad');
+	var pad1 = document.getElementById('pad1');
+	var pad2 = document.getElementById('pad2');
+	var pad3 = document.getElementById('pad3');
 
-    //http://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/3866442#3866442
-    function setEndOfContenteditable(contentEditableElement)
-    {
-        var range,selection;
-        if(document.createRange)
-        {
-            range = document.createRange();
-            range.selectNodeContents(contentEditableElement);
-            range.collapse(false);
-            selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
+	//http://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/3866442#3866442
+	function setEndOfContenteditable(contentEditableElement) {
+		var range, selection;
+		if(document.createRange) {
+			range = document.createRange();
+			range.selectNodeContents(contentEditableElement);
+			range.collapse(false);
+			selection = window.getSelection();
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
+	}
 
-        // else { IE < 8 }
-    }
+	function handleWindowActive() {
+		document.getElementById('pads').style.visibility='visible';
+		// setEndOfContenteditable(pad1);
+	}
+	function handleWindowFocus() {
+		if(typeof localStorage.textpad_buffer1 != 'undefined') {
+			pad1.innerHTML = localStorage.textpad_buffer1;
+		} else {
+			pad1.innerHTML = '';
+		}
+		if(typeof localStorage.textpad_buffer2 != 'undefined') {
+			pad2.innerHTML = localStorage.textpad_buffer2;
+		} else {
+			pad2.innerHTML = '';
+		}
+		if(typeof localStorage.textpad_buffer3 != 'undefined') {
+			pad3.innerHTML = localStorage.textpad_buffer3;
+		} else {
+			pad3.innerHTML = '';
+		}
+		// document.getElementById(pad1).focus();
+	}
 
-    function handleWindowActive() {
-        if( typeof localStorage.textpad_buffer != 'undefined') {
-            pad.innerHTML = localStorage.textpad_buffer;
-        } else {
-            localStorage.textpad_buffer = pad.innerHTML;
-        }
+	function saveProgress() {
+		localStorage.textpad_buffer1 = pad1.innerHTML;
+		localStorage.textpad_buffer2 = pad2.innerHTML;
+		localStorage.textpad_buffer3 = pad3.innerHTML;
+	}
 
-        setEndOfContenteditable(pad);
-    }
-
-    function handleWindowBlur() {
-        localStorage.textpad_buffer = pad.innerHTML;
-    }
-
-    function saveProgress() {
-        localStorage.textpad_buffer = pad.innerHTML;
-    }
-
-    window.addEventListener("focus", handleWindowActive);
-    window.addEventListener("blur", handleWindowBlur);
-    window.onbeforeunload = function() {
-        saveProgress();
-    };
+	window.addEventListener("focus", handleWindowActive);
+	window.addEventListener("blur", saveProgress);
+	document.onkeyup = function() {
+		saveProgress();
+	}
+	handleWindowFocus();
 
 };
